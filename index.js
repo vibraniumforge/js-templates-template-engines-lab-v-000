5e42be8d1e3793a2cd484ff4b312bc779b281b99
Greg Morgan <gregm9999@gmail.com>
2019-01-21 05:00:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ function postComment(){
   let text = document.getElementById("post-template");
   let commenter = document.getElementById('commentName').value;
  let comment = document.getElementById('commentText').value;
- let commentTemplate="
- <div id="comment"><p><%= comment %></p><p>Posted by:<span class="commenter"><%= commenter %></span></p></div>"
+ // let commentTemplate="
+ // <div id="comment"><p><%= comment %></p><p>Posted by:<span class="commenter"><%= commenter %></span></p></div>"
+  let commentTemplate = document.getElementById('comment-template').innerHTML;
 }
