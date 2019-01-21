9f94a7b7fed4bd3a80baf0e2b7216cbc06587596
Greg Morgan <gregm9999@gmail.com>
2019-01-21 05:21:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ function createPost() {
   let postTemplate=._template.getElementById("post-template").innerHTML;
   let commentTemplate=._template.getElementById("comments-template").innerHTML;
 
+  let pa
+
 
 
 
