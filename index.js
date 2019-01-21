89938c712c1d309e34ebfcabd10f733d277c24a0
Greg Morgan <gregm9999@gmail.com>
2019-01-21 05:01:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,17 +6,10 @@ function createPost() {
 function postComment(){
   let text = document.getElementById("post-template");
   let commenter = document.getElementById('commentName').value;
- let comment = document.getElementById('commentText').value;
- // let commentTemplate="
- // <div id="comment"><p><%= comment %></p><p>Posted by:<span class="commenter"><%= commenter %></span></p></div>"
+  let comment = document.getElementById('commentText').value;
   let commentTemplate = document.getElementById('comment-template').innerHTML;
   let templateFn = _.template(commentTemplate);
-
-let commentsDiv = document.getElementById('comments');
-
-//execute template function with JSON object for the interpolated values
-let templateHTML = templateFn({ comment: comment, commenter: commenter });
-
-//append rather than replace!
-commentsDiv.innerHTML += templateHTML;
+  let commentsDiv = document.getElementById('comments');
+  let templateHTML = templateFn({ comment: comment, commenter: commenter });
+  commentsDiv.innerHTML += templateHTML;
 }
