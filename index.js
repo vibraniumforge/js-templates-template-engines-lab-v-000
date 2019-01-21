0743c17e353c6d4a29f75fc6cf98274d716d4638
Greg Morgan <gregm9999@gmail.com>
2019-01-21 05:00:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,4 +10,13 @@ function postComment(){
  // let commentTemplate="
  // <div id="comment"><p><%= comment %></p><p>Posted by:<span class="commenter"><%= commenter %></span></p></div>"
   let commentTemplate = document.getElementById('comment-template').innerHTML;
+  let templateFn = _.template(commentTemplate);
+
+let commentsDiv = document.getElementById('comments');
+
+//execute template function with JSON object for the interpolated values
+let templateHTML = templateFn({ comment: comment, commenter: commenter });
+
+//append rather than replace!
+commentsDiv.innerHTML += templateHTML;
 }
